fd6b290e05e3d3051ac48eeb92a5525558a81ebc
Santiago Velosa <santiagovelosa@gmail.com>
2022-01-04 15:29:45 -0500

    Fix issues as requested during code review

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Calculator from './components/Calculator';
 import './styles.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      Calculator()
+      <Calculator />
     );
   }
 }
