b949c52ea100cf4312f2e3934c7f43816f07b3b8
Santiago Velosa <santiagovelosa@gmail.com>
2022-01-05 16:23:03 -0500

    Finish project's milestone

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,48 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { operand: {} };
+  }
+
+  printOn = (event) => {
+    const buttonName = event.target.textContent;
+    const { operand } = this.state;
+    this.setState({
+      operand: calculate(operand, buttonName),
+    });
   }
 
   render() {
+    const { operand } = this.state;
     return (
       <div className="calculator-grid">
-        <div className="output">0</div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operator">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operator">x</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operator">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operator">+</button>
-        <button type="button" className="occupy-two">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operator">=</button>
+        <div className="output">
+          {operand.next}
+          {operand.operation}
+          {operand.total}
+        </div>
+        <button type="button" onClick={this.printOn}>AC</button>
+        <button type="button" onClick={this.printOn}>+/-</button>
+        <button type="button" onClick={this.printOn}>%</button>
+        <button type="button" className="operator" onClick={this.printOn}>÷</button>
+        <button type="button" onClick={this.printOn}>7</button>
+        <button type="button" onClick={this.printOn}>8</button>
+        <button type="button" onClick={this.printOn}>9</button>
+        <button type="button" className="operator" onClick={this.printOn}>x</button>
+        <button type="button" onClick={this.printOn}>4</button>
+        <button type="button" onClick={this.printOn}>5</button>
+        <button type="button" onClick={this.printOn}>6</button>
+        <button type="button" className="operator" onClick={this.printOn}>-</button>
+        <button type="button" onClick={this.printOn}>1</button>
+        <button type="button" onClick={this.printOn}>2</button>
+        <button type="button" onClick={this.printOn}>3</button>
+        <button type="button" className="operator" onClick={this.printOn}>+</button>
+        <button type="button" className="occupy-two" onClick={this.printOn}>0</button>
+        <button type="button" onClick={this.printOn}>.</button>
+        <button type="button" className="operator" onClick={this.printOn}>=</button>
       </div>
     );
   }
