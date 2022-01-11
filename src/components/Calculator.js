1e1e699727f63883c3678a9e3b99828746827da3
Santiago Velosa <santiagovelosa@gmail.com>
2022-01-11 16:54:58 -0500

    Implement routing to finish website

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,33 +10,37 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        {operand.total}
+    <div className="container">
+      <h1 className="calculator-title">Math Magicians</h1>
+      <h2 className="calculator-subtitle">Let&apos;s do some math!</h2>
+      <div className="calculator-grid">
+        <div className="output">
+          {operand.total}
         &nbsp;
-        {operand.operation}
+          {operand.operation}
         &nbsp;
-        {operand.next}
+          {operand.next}
+        </div>
+        <button type="button" onClick={printOn}>AC</button>
+        <button type="button" onClick={printOn}>+/-</button>
+        <button type="button" onClick={printOn}>%</button>
+        <button type="button" className="operator" onClick={printOn}>÷</button>
+        <button type="button" onClick={printOn}>7</button>
+        <button type="button" onClick={printOn}>8</button>
+        <button type="button" onClick={printOn}>9</button>
+        <button type="button" className="operator" onClick={printOn}>x</button>
+        <button type="button" onClick={printOn}>4</button>
+        <button type="button" onClick={printOn}>5</button>
+        <button type="button" onClick={printOn}>6</button>
+        <button type="button" className="operator" onClick={printOn}>-</button>
+        <button type="button" onClick={printOn}>1</button>
+        <button type="button" onClick={printOn}>2</button>
+        <button type="button" onClick={printOn}>3</button>
+        <button type="button" className="operator" onClick={printOn}>+</button>
+        <button type="button" className="occupy-two" onClick={printOn}>0</button>
+        <button type="button" onClick={printOn}>.</button>
+        <button type="button" className="operator" onClick={printOn}>=</button>
       </div>
-      <button type="button" onClick={printOn}>AC</button>
-      <button type="button" onClick={printOn}>+/-</button>
-      <button type="button" onClick={printOn}>%</button>
-      <button type="button" className="operator" onClick={printOn}>÷</button>
-      <button type="button" onClick={printOn}>7</button>
-      <button type="button" onClick={printOn}>8</button>
-      <button type="button" onClick={printOn}>9</button>
-      <button type="button" className="operator" onClick={printOn}>x</button>
-      <button type="button" onClick={printOn}>4</button>
-      <button type="button" onClick={printOn}>5</button>
-      <button type="button" onClick={printOn}>6</button>
-      <button type="button" className="operator" onClick={printOn}>-</button>
-      <button type="button" onClick={printOn}>1</button>
-      <button type="button" onClick={printOn}>2</button>
-      <button type="button" onClick={printOn}>3</button>
-      <button type="button" className="operator" onClick={printOn}>+</button>
-      <button type="button" className="occupy-two" onClick={printOn}>0</button>
-      <button type="button" onClick={printOn}>.</button>
-      <button type="button" className="operator" onClick={printOn}>=</button>
     </div>
   );
 };
